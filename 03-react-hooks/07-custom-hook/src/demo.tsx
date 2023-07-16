955539c6922413b8ca1c64e2b17596672e12bd42
Braulio Diez Botella <braulio.diez@gmail.com>
2023-07-16 21:34:32 +0200

    wip

--- 03-react-hooks/07-custom-hook/src/demo.tsx
+++ 03-react-hooks/07-custom-hook/src/demo.tsx
@@ -4,17 +4,17 @@ const useUserCollection = () => {
   const [filter, setFilter] = React.useState("");
   const [userCollection, setUserCollection] = React.useState([]);
 
-  // Load full list when the component gets mounted and filter gets updated
-  const loadUsers = () =>
+  const loadUsers = () => {
     fetch(`https://jsonplaceholder.typicode.com/users?name_like=${filter}`)
       .then((response) => response.json())
       .then((json) => setUserCollection(json));
+  };
 
   return { userCollection, loadUsers, filter, setFilter };
 };
 
 export const MyComponent = () => {
-  const { userCollection, loadUsers, filter, setFilter } = useUserCollection();
+  const { userCollection, filter, setFilter, loadUsers } = useUserCollection();
 
   React.useEffect(() => {
     loadUsers();
